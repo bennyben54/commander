edc3c37da2de5b997112775ef5d0a2ff2ab8c7af
Benny Ben <bennyben54@gmail.com>
2019-08-25 23:40:22 +0200

    [FEAT](command) rest + service layer for launching commands

--- src/main/java/com/beo/commander/rest/CommanderRestController.java
+++ src/main/java/com/beo/commander/rest/CommanderRestController.java
@@ -1,34 +1,41 @@
 package com.beo.commander.rest;
 
+import com.beo.commander.service.CommanderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/command")
 public class CommanderRestController {
 
+    private final CommanderService commanderService;
+
     @Autowired
-    public CommanderRestController() {
+    public CommanderRestController(CommanderService commanderService) {
+        this.commanderService = commanderService;
     }
 
     @Secured({"ROLE_ADMIN"})
-    @PostMapping(value = "/toto")
-    public ResponseEntity<String> toto(Authentication authentication) {
-        return ResponseEntity.ok("toto " + authentication.getName());
+    @PostMapping(value = "/launch")
+    public ResponseEntity<Integer> launch(Authentication authentication) {
+        return ResponseEntity.ok(commanderService.launchChromium());
     }
 
-    @Secured({"ROLE_ADMIN", "ROLE_USER"})
-    @GetMapping(value = "/titi")
-    public ResponseEntity<String> titi(Authentication authentication) {
-        return ResponseEntity.ok("titi " + authentication.getName());
+    @Secured({"ROLE_ADMIN"})
+    @PostMapping(value = "/stop")
+    public ResponseEntity<Integer> stop(Authentication authentication) {
+        return ResponseEntity.ok(commanderService.stopChromium());
     }
+
+//    @Secured({"ROLE_ADMIN", "ROLE_USER"})
+//    @GetMapping(value = "/titi")
+//    public ResponseEntity<String> titi(Authentication authentication) {
+//        return ResponseEntity.ok("titi " + authentication.getName());
+//    }
 
 }
